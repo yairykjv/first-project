0d278e81968337af779611661d1c1b8c4c5c25a5
yairr <yairrr20000@gmail.com>
2025-12-07 21:54:20 +0200

    first commit

--- project/src/main/java/com/first/project/controller/Hellocontroller.java
+++ project/src/main/java/com/first/project/controller/Hellocontroller.java
@@ -1,21 +1,17 @@
 package com.first.project.controller;
 
-import org.springframework.http.HttpStatus;
+import com.first.project.object.Hors;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
-public class Hellocontroller {
+class HorsController {
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public ResponseEntity<?> hello(){
-        return new ResponseEntity<>("Hello World!", HttpStatus.OK);
-    }
-    @RequestMapping(value = "/bye", method = RequestMethod.GET)
-    public ResponseEntity<?> bye(){
-        return new ResponseEntity<>("bye World!", HttpStatus.OK);
+    @GetMapping("/api/hors")
+    public ResponseEntity<String> getHorsName() {
+        Hors h = new Hors();
+        return ResponseEntity.ok(h.getName());
     }
 }
+
---
+++ project/src/main/java/com/first/project/object/Hors.java
@@ -0,0 +1,10 @@
+package com.first.project.object;
+
+import lombok.Data;
+
+@Data
+public class Hors {
+    private String Name = "toya";
+    private int age = 5;
+    private boolean isFemale = true;
+}
